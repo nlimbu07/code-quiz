16bc468010efe19db7257b479fa7b0fc837c4de7
Niraj Limbu <nlimbu@yahoo.com>
2021-11-27 23:12:36 -0500

    updated quiz-function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var choice3 = document.querySelector('#choice3');
 var choice4 = document.querySelector('#choice4');
 var timer = document.querySelector('#time');
 var messageEl = document.querySelector('#message');
+var startBtnEl = document.querySelector('#startBtn');
+var questionEl = document.querySelector('#question');
 
 // Quiz state variables
 var currentQuestionIndex = 0;
@@ -27,3 +29,125 @@ function startQuiz() {
   }, 1000);
   return score;
 }
+
+function getQuestion() {
+  startBtnEl.hidden = true;
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    questionEl.textContent = questions[currentQuestionIndex]['title'];
+    choice1.textContent = questions[currentQuestionIndex]['choices'][0];
+    choice2.textContent = questions[currentQuestionIndex]['choices'][1];
+    choice3.textContent = questions[currentQuestionIndex]['choices'][2];
+    choice4.textContent = questions[currentQuestionIndex]['choices'][3];
+  }
+}
+
+function quizEnd() {
+  var scoreTag = document.createElement('h1');
+  scoreTag.id = 'score';
+  var inputTag = document.createElement('input');
+  inputTag.id = 'input-field';
+  var submitBtn = document.createElement('button');
+  score += seconds * 0.1;
+  // hide question section
+  choice1.remove();
+  choice2.remove();
+  choice3.remove();
+  choice4.remove();
+  // hide message
+  message.remove();
+
+  questionEl.textContent = 'All Done!';
+  document.body.children[1].appendChild(scoreTag);
+  document.getElementById('score').textContent = 'Your final score: ' + score;
+  document.body.children[1].appendChild(inputTag);
+  submitBtn.textContent = 'Submit';
+  document.body.children[1].appendChild(submitBtn);
+
+  submitBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    var highScoreText = new Object();
+    highScoreText.name = inputTag.value.trim();
+    highScoreText.newScore = score;
+    storedScores(highScoreText);
+    window.location.href = 'scores.html';
+  });
+}
+
+function storeScores(highScoreText) {
+  tempArray = JSON.parse(localStorage.getItem('scores'));
+  if (tempArray === null) {
+    scoreList.push(highScoreText);
+    localStorage.setItem('scores', JSON.stringify(scoreList));
+  } else {
+    tempArray.push(highScoreText);
+    localStorage.setItem('scores', JSON.stringify(tempArray));
+  }
+}
+
+startBtnEl.addEventListener('click', function () {
+  messageEl.textContent = '';
+});
+startBtnEl.addEventListener('click', startQuiz);
+startBtnEl.addEventListener('click', getQuestion);
+
+choice1.addEventListener('click', function () {
+  if (
+    questions[currentQuestionIndex]['choices'][0] ===
+    questions[currentQuestionIndex]['answer']
+  ) {
+    messageEl.textContent = 'Correct';
+    score++;
+  } else {
+    messageEl.textContent = 'Wrong!';
+    seconds -= 10;
+  }
+  currentQuestionIndex++;
+  getQuestion();
+});
+
+choice2.addEventListener('click', function () {
+  if (
+    questions[currentQuestionIndex]['choices'][1] ===
+    questions[currentQuestionIndex]['answer']
+  ) {
+    messageEl.textContent = 'Correct!';
+    score++;
+  } else {
+    messageEl.textContent = 'Wrong!';
+    seconds -= 10;
+  }
+  currentQuestionIndex++;
+  getQuestion();
+});
+
+choice3.addEventListener('click', function () {
+  if (
+    questions[currentQuestionIndex]['choices'][2] ===
+    questions[currentQuestionIndex]['answer']
+  ) {
+    messageEl.textContent = 'Correct!';
+    score++;
+  } else {
+    messageEl.textContent = 'Wrong!';
+    seconds -= 10;
+  }
+  currentQuestionIndex++;
+  getQuestion();
+});
+
+choice4.addEventListener('click', function () {
+  if (
+    questions[currentQuestionIndex]['choices'][3] ===
+    questions[currentQuestionIndex]['answer']
+  ) {
+    messageEl.textContent = 'Correct!';
+    score++;
+  } else {
+    messageEl.textContent = 'Wrong!';
+    seconds -= 10;
+  }
+  currentQuestionIndex++;
+  getQuestion();
+});
